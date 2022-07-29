61e2c4eed93116a9010a3cc38299ced8a5d39b64
Manuel Antonio Ciprian Berra <mcipriab@everis.com>
2022-07-29 21:24:44 +0200

    Added helm_installed_resources.tf and istio chart within it

---
+++ helm_installed_resources.tf
@@ -0,0 +1,42 @@
+resource "helm_release" "example" {
+  name       = "istio"
+  repository = "https://charts.cloudposse.com/incubator/"
+  chart      = "istio"
+  version    = "1.1.0"
+  namespace  = "istio"
+
+  set {
+    name  = "ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "gateways.istio-ingressgateway.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "gateways.istio-egressgateway.enabled"
+    value = "true"
+  }
+  set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+  set {
+    name  = "prometheus.enabled"
+    value = "true"
+  }
+  set {
+    name  = "tracing.enabled"
+    value = "true"
+  }
+  set {
+    name  = "servicegraph.enabled"
+    value = "true"
+  }
+  set {
+    name  = "kiali.enabled"
+    value = "true"
+  }
+}
